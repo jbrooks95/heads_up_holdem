b9c918ea2785a8f2bb3fabc98552d2b3a101c663
James Brooks <jamesbrooks1@u.boisestate.edu>
2020-04-12 17:48:32 -0600

    add four of a kind check

--- src/hand.c
+++ src/hand.c
@@ -6,34 +6,91 @@
 //function prototypes
 void sort_ascending(hand* hand);
 int is_sorted_ascending(hand* hand);
+int compare_hand_values(int hand1_value, int hand2_value);
 
 hand* find_best_hand(card* cards[])
 {
     return 0;
 }
 
+//returns 1 if hand1 > hand2
+//returns -1 if hand2 > hand1
+//returns 0 if hand1 = hand2
+//this function does things twice for simplicity and readability
 int compare_hands(hand* hand1, hand* hand2)
 {
+    //check for straight flush
+    int value1 = is_straight_flush(hand1);
+    int value2 = is_straight_flush(hand2);
+    if(value1 || value2) return compare_hand_values(value1, value2);
+
+    //check for 4 of a kind
+    //check for full house
     //check for flush
     //check for straight    
-    //check for 4 of a kind
     //check for 3 of a kind
+    //check for 2 pair
     //check for pair
     //check for high card
 }
 
+//return 1 if hand1 > hand2
+//return -1 if hand2 > hand1
+//return 0 if hand1 = hand2
+int compare_hand_values(int hand1_value, int hand2_value)
+{
+    if(hand1_value > hand2_value) return 1;
+    else if(hand1_value < hand2_value) return -1;
+    else return 0;
+}
+
+int is_four_of_a_kind(hand* hand)
+{
+    int first = hand->cards[0]->value;
+    int second = hand->cards[1]->value;
+    int i;
+    int count = 1;
+    for(i = 1; i < 5; i++)
+    {
+        if(hand->cards[i]->value == first) count++;
+    }
+
+    if(count == 4) return first;
+
+    count = 1;
+    for(i = 2; i < 5; i++)
+    {
+        if(hand->cards[i]->value == second) count++;
+    }
+
+    if(count == 4) return second;
+
+    return 0;
+}
+
+//returns value of highest card in straight
+int is_straight_flush(hand* hand)
+{
+    int flush_value = is_flush(hand);
+    int straight_value = is_straight(hand);
+    if(flush_value && straight_value) return straight_value;
+    else return 0;
+}
+
+//returns value of highest card in flush
 int is_flush(hand* hand)
 {
+    sort_ascending(hand);
     int i;
-    fprintf(stderr, "%d", i);
     int first_suit = hand->cards[0]->suit;
     for(i = 1; i < 5; i++)
     {
         if(hand->cards[i]->suit != first_suit) return 0;
     }
-    return 1;
+    return hand->cards[4]->value;
 }
 
+//returns value of highest card in straight
 int is_straight(hand* hand)
 {
     sort_ascending(hand);
@@ -44,7 +101,7 @@ int is_straight(hand* hand)
         if(i == 4)
         {
             //check for ace if cards start with 2
-            if(hand->cards[4]->value == 13 && hand->cards[0]->value == 2) return 1;
+            if(hand->cards[4]->value == 13 && hand->cards[0]->value == 2) return 5;
         }
         if(current + 1 != hand->cards[i]->value)
         {
@@ -52,7 +109,7 @@ int is_straight(hand* hand)
         }
         current = hand->cards[i]->value;
     }
-    return 1;
+    return hand->cards[4]->value;
 }
 
 void sort_ascending(hand* hand)
--- src/tester.c
+++ src/tester.c
@@ -8,30 +8,28 @@ int main()
 {
     printf("hey\n");
 
-    card* c1 = create_card(2, 2);
-    card* c2 = create_card(13, 2);
+    card* c1 = create_card(3, 2);
+    card* c2 = create_card(3, 2);
     card* c3 = create_card(3, 2);
-    card* c4 = create_card(4, 2);
-    card* c5 = create_card(2, 2);
+    card* c4 = create_card(3, 2);
+    card* c5 = create_card(5, 2);
 
     hand* h = malloc(sizeof(hand));
-    printf("hey3\n");
     h->cards[0] = c1;
     h->cards[1] = c2;
     h->cards[2] = c3;
     h->cards[3] = c4;
     h->cards[4] = c5;
 
-    int isflush = is_flush(h);
-    printf("is flush %d\n", isflush);
-
-    int isstraight = is_straight(h);
-    printf("is straight %d\n", isstraight);
+    printf("is straight flush %d \n", is_straight_flush(h));
+    printf("is four of a kind %d \n", is_four_of_a_kind(h));
+    printf("is flush %d \n", is_flush(h));
+    printf("is straight %d \n", is_straight(h));
 
     free(h);
 
     deck* d = create_deck();
-    print_deck(d);
+    //print_deck(d);
 
     printf("hey\n");
     printf("hey\n");
